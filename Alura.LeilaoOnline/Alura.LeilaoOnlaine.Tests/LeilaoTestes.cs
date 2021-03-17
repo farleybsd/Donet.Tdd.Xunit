c5c86a8a436ba5ba9cdebfe1d27bc539d353ea0f
farleybsd <farley.t.i@hotmail.com>
2021-03-17 20:26:30 -0300

    Teorizando com xUnit

--- Alura.LeilaoOnline/Alura.LeilaoOnlaine.Tests/LeilaoTestes.cs
+++ Alura.LeilaoOnline/Alura.LeilaoOnlaine.Tests/LeilaoTestes.cs
@@ -5,103 +5,35 @@ namespace Alura.LeilaoOnlaine.Tests
 {
    public class LeilaoTestes
     {
-        [Fact] //Fato
-        public void LeilaoComTresClientes()
-        {
-            //Arranjo - cenário de entrada
-           //Dado Leilao com 3 clientes e lances realizados por eles
-            var leilao = new Leilao("Van Gogh");
-            var fulano = new Interessada("Fulano", leilao);
-            var maria = new Interessada("Maria", leilao);
-            var beltrano = new Interessada("beltrano",leilao);
 
-            leilao.RecebeLance(fulano, 800);
-            leilao.RecebeLance(maria, 900);
-            leilao.RecebeLance(fulano, 1000);
-            leilao.RecebeLance(maria, 990);
-            leilao.RecebeLance(beltrano, 1400);
-
-            // Act - método sobre teste
-           //  Quando o pegrão/Leilão termima
-            leilao.TerminaPregao();
-
-            //Assert resultado esperado
-           //Então o valor esperado é o maior valor dado
-          //E o cliente ganhador é o que deu o maior lance
-            var valoresperado = 1400;
-            var valorObtifo = leilao.Ganhador.Valor;
-            Assert.Equal(valoresperado, valorObtifo);
-            Assert.Equal(beltrano,leilao.Ganhador.Cliente);
-        }
-
-        [Fact] // Fato
-        public void LeilaoComLanceOrdenadasPorValor()
+        [Theory] //Teoria teste que sao verdades para um tipo particular de dado
+        [InlineData(1200, new double[] { 800, 900, 1000, 1200 })] // Passando Dado
+        [InlineData(1000, new double[] { 800, 900, 1000, 990 })] // Passando Dado
+        [InlineData(800, new double[] { 800})] // Passando Dados
+        public  void LeilaoComVariosLances(double valoresperado,double[] ofertas)
         {
             //Arranjo - cenário de entrada
             var leilao = new Leilao("Van Gogh");
             var fulano = new Interessada("Fulano", leilao);
-            var maria = new Interessada("Maria", leilao);
-
-            leilao.RecebeLance(fulano, 800);
-            leilao.RecebeLance(maria, 900);
-            leilao.RecebeLance(fulano, 1000);
-            leilao.RecebeLance(maria, 990);
-
-            // Act - método sobre teste
-            leilao.TerminaPregao();
-
-            //Assert resultado esperado
-            var valoresperado = 1000;
-            var valorObtifo = leilao.Ganhador.Valor;
-            Assert.Equal(valoresperado, valorObtifo);
-        }
-
-        [Fact] // FATO
-        public  void LeilaoComVariosLances()
-        {
-            //Arranjo - cenário de entrada
-            var leilao = new Leilao("Van Gogh");
-            var fulano = new Interessada("Fulano", leilao);
-            var maria = new Interessada("Maria", leilao);
-
-            leilao.RecebeLance(fulano, 800);
-            leilao.RecebeLance(maria, 900);
-            leilao.RecebeLance(maria, 990);
-            leilao.RecebeLance(fulano, 1000);
             
 
+            foreach (var valor in ofertas)
+            {
+                leilao.RecebeLance(fulano, valor);
+               
+            }
+
             // Act - método sobre teste
             leilao.TerminaPregao();
 
             //Assert resultado esperado
-            var valoresperado = 1000;
             var valorObtifo = leilao.Ganhador.Valor;
             Assert.Equal(valoresperado, valorObtifo);
 
 
         }
 
-        [Fact] // FATO
-        public void LeilaoApenasComUmLance()
-        {
-            //Arranjo - cenário de entrada
-            var leilao = new Leilao("Van Gogh");
-            var fulano = new Interessada("Fulano", leilao);
-
-
-            leilao.RecebeLance(fulano, 800);
-
-            // Act - método sobre teste
-            leilao.TerminaPregao();
-
-            //Assert resultado esperado
-            var valoresperado = 800;
-            var valorObtifo = leilao.Ganhador.Valor;
-            Assert.Equal(valoresperado, valorObtifo);
-
-        }
-
-        [Fact] // Fato
+        [Fact] // Fato sao sempre teste verdades que testa condiçoes e variantes
         public void LeilaoSemLance()
         {
 
