19d3772f739d47435cc4c7314cc3fef4da628fb3
farleybsd <farley.t.i@hotmail.com>
2021-03-16 23:37:00 -0300

    Estrutura de um teste automatizado

--- Alura.LeilaoOnline/Alura.LeilaoOnlaine.ConsoleApp/Program.cs
+++ Alura.LeilaoOnline/Alura.LeilaoOnlaine.ConsoleApp/Program.cs
@@ -5,22 +5,47 @@ namespace Alura.LeilaoOnlaine.ConsoleApp
 {
     class Program
     {
-        static void Main()
+        private static void LeilaoComVariosLances()
         {
+            //Arranjo - cenário de entrada
             var leilao = new Leilao("Van Gogh");
             var fulano = new Interessada("Fulano", leilao);
             var maria = new Interessada("Maria", leilao);
 
-            leilao.RecebeLance(fulano,800);
+            leilao.RecebeLance(fulano, 800);
             leilao.RecebeLance(maria, 900);
             leilao.RecebeLance(fulano, 1000);
             leilao.RecebeLance(maria, 990);
 
+            // Act - método sobre teste
             leilao.TerminaPregao();
 
+            //Assert resultado esperado
             Console.WriteLine(leilao.Ganhador.Valor);
 
-            Console.ReadKey();
+            
+        }
+
+        private static void LeilaoApenasComUmLance()
+        {
+            //Arranjo - cenário de entrada
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+          
+
+            leilao.RecebeLance(fulano, 800);
+
+            // Act - método sobre teste
+            leilao.TerminaPregao();
+
+            //Assert resultado esperado
+            Console.WriteLine(leilao.Ganhador.Valor);
+
+        }
+        static void Main()
+        {
+            LeilaoComVariosLances();
+            LeilaoApenasComUmLance();
         }
     }
 }
--- Alura.LeilaoOnline/Alura.LeilaoOnline.Core/Leilao.cs
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Core/Leilao.cs
@@ -27,7 +27,9 @@ namespace Alura.LeilaoOnline.Core
 
         public void TerminaPregao()
         {
-            Ganhador = Lances.Last();
+            Ganhador = Lances
+                       .OrderBy(l => l.Valor)
+                       .Last();
         }
     }
 }
