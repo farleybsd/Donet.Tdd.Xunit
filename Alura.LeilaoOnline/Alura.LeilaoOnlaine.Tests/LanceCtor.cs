46e95429bed2460ed598dcd28f482dd5882e2663
farleybsd <farley.t.i@hotmail.com>
2021-03-29 12:17:09 -0300

    Testando outra exceção

---
+++ Alura.LeilaoOnline/Alura.LeilaoOnlaine.Tests/LanceCtor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Alura.LeilaoOnline.Core;
+using Xunit;
+
+namespace Alura.LeilaoOnlaine.Tests
+{
+    public class LanceCtor
+    {
+        [Fact]
+        public void LancaArgumentExceptionDadoValorNegativo()
+        {
+            // Arranjo
+            var valornegativo = -100;
+
+            //Assert
+            Assert.Throws<System.ArgumentException>(
+                //Act
+                () => new Lance(null, valornegativo)
+
+                ); ;
+        }
+    }
+}
